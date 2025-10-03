50dc1cc81dd79070d664e6cc4df1745e89a2d4fe
Berthose Fin <berthosefin@gmail.com>
2025-10-03 07:06:43 +0300

    Clean nvim config

--- home/.config/nvim/lua/config/options.lua
+++ home/.config/nvim/lua/config/options.lua
@@ -1,6 +1,3 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.g.lazyvim_php_lsp = "intelephense"
-vim.g.lazyvim_eslint_auto_format = true
-vim.g.lazyvim_prettier_needs_config = false
