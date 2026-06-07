d601b75e5d3fa7c5de27e9c16d11090fcf672a00
Berthose Fin <berthosefin@gmail.com>
2026-06-07 10:17:43 +0300

    chore: swap monitor positions and disable custom tmux prefix

--- home/.config/hypr/hyprland.lua
+++ home/.config/hypr/hyprland.lua
@@ -10,14 +10,14 @@ local myColors = dofile(os.getenv("HOME") .. "/.cache/wal/colors-hyprland.lua")
 hl.monitor({
 	output = "eDP-1",
 	mode = "1366x768@60",
-	position = "1360x0",
+	position = "0x0",
 	scale = "1",
 })
 
 hl.monitor({
 	output = "HDMI-A-1",
 	mode = "1360x768@60",
-	position = "0x0",
+	position = "1366x0",
 	scale = "1",
 })
 
