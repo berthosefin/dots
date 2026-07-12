6c162f3f76ec3aaca2410f9f12205e950471e390
Berthose Fin <berthosefin@gmail.com>
2026-07-12 18:01:35 +0300

    fix: allow hyprlock screencopy

--- home/.config/hypr/hyprland.lua
+++ home/.config/hypr/hyprland.lua
@@ -88,6 +88,7 @@ hl.config({
   },
 })
 
+hl.permission({ binary = "/usr/bin/hyprlock", type = "screencopy", mode = "allow" })
 hl.permission({ binary = "/usr/bin/grim", type = "screencopy", mode = "allow" })
 hl.permission({ binary = "/usr/(lib|libexec|lib64)/xdg-desktop-portal-hyprland", type = "screencopy", mode = "allow" })
 
