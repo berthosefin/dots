15adc4a67b265890bb3bb3a55f2ae469f99db2a9
Berthose Fin <berthosefin@gmail.com>
2026-07-13 17:56:39 +0300

    refactor: adjust gap and border sizing

--- home/.config/hypr/hyprland.lua
+++ home/.config/hypr/hyprland.lua
@@ -98,7 +98,7 @@ hl.permission({ binary = "/usr/(lib|libexec|lib64)/xdg-desktop-portal-hyprland",
 
 hl.config({
   general = {
-    gaps_in = 1,
+    gaps_in = 2,
     gaps_out = 4,
 
     border_size = 2,
@@ -189,20 +189,20 @@ hl.animation({ leaf = "workspacesOut", enabled = true, speed = 1.94, bezier = "a
 hl.animation({ leaf = "zoomFactor", enabled = true, speed = 7, bezier = "quick" })
 
 -- "Smart gaps" / "No gaps when only"
-hl.workspace_rule({ workspace = "w[tv1]", gaps_out = 0, gaps_in = 0 })
-hl.workspace_rule({ workspace = "f[1]", gaps_out = 0, gaps_in = 0 })
-hl.window_rule({
-  name = "no-gaps-wtv1",
-  match = { float = false, workspace = "w[tv1]" },
-  border_size = 0,
-  rounding = 0,
-})
-hl.window_rule({
-  name = "no-gaps-f1",
-  match = { float = false, workspace = "f[1]" },
-  border_size = 0,
-  rounding = 0,
-})
+-- hl.workspace_rule({ workspace = "w[tv1]", gaps_out = 0, gaps_in = 0 })
+-- hl.workspace_rule({ workspace = "f[1]", gaps_out = 0, gaps_in = 0 })
+-- hl.window_rule({
+--   name = "no-gaps-wtv1",
+--   match = { float = false, workspace = "w[tv1]" },
+--   border_size = 0,
+--   rounding = 0,
+-- })
+-- hl.window_rule({
+--   name = "no-gaps-f1",
+--   match = { float = false, workspace = "f[1]" },
+--   border_size = 0,
+--   rounding = 0,
+-- })
 
 -- Layout
 hl.config({
