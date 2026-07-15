37532f5e423ca8009f32f0b0819a90808245a79f
Berthose Fin <berthosefin@gmail.com>
2026-07-15 18:41:00 +0300

    refactor: autocommands for colorscheme and whitespace
    Update the matugen reload trigger and replace the deprecated substitute
    command for stripping trailing whitespace with a safer API-based
    implementation.

--- home/.config/nvim/lua/config/autocmds.lua
+++ home/.config/nvim/lua/config/autocmds.lua
@@ -2,7 +2,7 @@
 vim.api.nvim_create_autocmd('Signal', {
   pattern = 'SIGUSR1',
   callback = function()
-    require('matugen').reload()
+    require('matugen').load()
   end,
 })
 
@@ -21,7 +21,11 @@ vim.api.nvim_create_autocmd('BufWritePre', {
   pattern = '*',
   callback = function()
     local save = vim.fn.winsaveview()
-    vim.fn.gsub([[\s\+$]], '', '')
+    local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+    for i, line in ipairs(lines) do
+      lines[i] = line:gsub('%s+$', '')
+    end
+    vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
     vim.fn.winrestview(save)
   end,
 })
