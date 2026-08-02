fca1d5b001e4913c35581f90380d9d47568e484d
Berthose Fin <berthosefin@gmail.com>
2026-08-02 09:45:48 +0300

    refactor(battery): replace hypr-timer script with batsignal

--- home/.config/hypr/hyprland.lua
+++ home/.config/hypr/hyprland.lua
@@ -55,7 +55,6 @@ hl.on("hyprland.start", function()
   hl.exec_cmd("hypridle")
   hl.exec_cmd("wl-paste --type text --watch cliphist store")
   hl.exec_cmd("wl-paste --type image --watch cliphist store")
-  hl.exec_cmd(scripts .. "/hypr-timer.sh")
 end)
 
 hl.on("monitor.added", function()
